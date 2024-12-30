7d72f159cc6deb5154a60e535fd06538e93d1d19
ShaiBrin <Ishr_Shai@live.ca>
2024-12-30 11:33:15 -0500

    refac: Removed output:export from next config js
    Error: export const dynamic = "force-static"/export const revalidate not configured on route "/api" with "output: export". See more info here: https://nextjs.org/docs/advanced-features/static-html-export
    
    Solution: The files in api are requesting dynamically while it assumed that the export is SSG meaning static, by removing the output export  we can have dynamic generation

--- my-osdk-app/src/app/costs/page.tsx
+++ my-osdk-app/src/app/costs/page.tsx
@@ -1,7 +1,6 @@
 "use client";
-import { Patient, PatientMedication } from "@hospital-osdk/sdk";
+import { PatientMedication } from "@hospital-osdk/sdk";
 import { Osdk } from "@osdk/client";
-import { useState, useEffect } from "react";
 import CostsLayout from "./layout";
 import client from "@/lib/client";
 
