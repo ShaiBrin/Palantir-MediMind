7aa7d01124d6a5bc0d766d3a807e18b922fa98c6
admin <ishraq-mahmud.shabab.1@ens.etsmtl.ca>
2024-12-30 10:14:46 -0500

    feat: Changed the main page to include client interface options

--- my-osdk-app/src/app/page.tsx
+++ my-osdk-app/src/app/page.tsx
@@ -1,10 +1,9 @@
 "use client";
 import { $Objects, $Actions, $Queries, PatientMedication } from "@hospital-osdk/sdk";
 import useAuthenticated from "@/lib/useAuthenticated";
-import css from "./page.module.css";
 import client from "@/lib/client";
 import { Osdk } from "@osdk/client";
-import { Patient } from "@hospital-osdk/sdk";
+import HomePageButtons from "./components/ui/HomePageButtons";
 
 const responseNoErrorWrapper: Osdk.Instance<PatientMedication> = await client(PatientMedication).fetchOne(2);
 
@@ -16,53 +15,9 @@ function Home() {
     return null;
   }
 
-  const objectApiNames = Object.keys($Objects);
-  const actionApiNames = Object.keys($Actions);
-  const queryApiNames = Object.keys($Queries);
 
   return (
-    <div>
-      <h1>{"I'm " + authenticated}</h1>
-      <p>
-        Welcome to your Ontology SDK! Try using any of the following methods
-        now.
-      </p>
-      <h1>
-        {responseNoErrorWrapper?.age}
-      </h1>
-      <h3>
-        {/* Ontology Object : {osdkProject.} */}
-      </h3>
-      <div className={css.methods}>
-        <div>
-          <h2>Objects ({objectApiNames.length})</h2>
-          {objectApiNames.map((objectApiName) => (
-            <pre key={objectApiName}>
-              $Objects.{objectApiName}
-              $Objects.{"objectApiName"}
-            </pre>
-          ))}
-        </div>
-        <div>
-          <h2>Actions ({actionApiNames.length})</h2>
-          {actionApiNames.map((actionApiName) => (
-            <pre key={actionApiName}>
-              $Actions.{actionApiName}
-            </pre>
-          ))}
-        </div>
-        <div>
-          <h2>Queries ({queryApiNames.length})</h2>
-          {queryApiNames.map((queryApiName) => (
-            <pre key={queryApiName}>
-              $Queries.{queryApiName}
-            </pre>
-          ))}
-        </div>
-        <div>
-        </div>
-      </div>
-    </div>
+    <HomePageButtons />
   );
 }
 
